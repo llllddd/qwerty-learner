54cfa109f5a3c7ac552eb78626e07ac361ce1c24
llllddd <tt861239947@gmail.com>
2026-02-06 01:42:11 +0000

    fix: change hotkey

--- src/pages/Typing/components/WordPanel/index.tsx
+++ src/pages/Typing/components/WordPanel/index.tsx
@@ -104,7 +104,7 @@ export default function WordPanel() {
   )
 
   useHotkeys(
-    'ArrowLeft',
+    'Ctrl + Shift + ArrowLeft',
     (e) => {
       e.preventDefault()
       onSkipWord('prev')
@@ -113,7 +113,7 @@ export default function WordPanel() {
   )
 
   useHotkeys(
-    'ArrowRight',
+    'Ctrl + Shift + ArrowRight',
     (e) => {
       e.preventDefault()
       onSkipWord('next')
